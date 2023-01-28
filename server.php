d3a8e3a0bd8d0927ebdcfd6b010ece4126132b04
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-28 19:11:55 +0100

    correct data path

--- server.php
+++ server.php
@@ -1,7 +1,9 @@
 <?php
-$jsonData = file_get_contents(__DIR__ . "./data.json");
+$jsonData = file_get_contents(__DIR__ . "/data.json");
 $discList = json_decode($jsonData, true);
 
 header('Content-Type: application/json');
 
 echo json_encode($discList);
+
+?>
